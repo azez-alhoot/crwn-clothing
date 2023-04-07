d6646e32d5677ea133f2fb84ff4f78a1b4e43d43
Azez Alhoot <azezalhoot82@yahoo.com>
2023-04-07 11:37:00 +0300

    add signIn page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 import { Routes, Route } from "react-router-dom";
 
 
@@ -9,6 +10,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />}/>
+        <Route path="sign-in" element={<SignIn />}/>
       </Route>
     </Routes>
   );
